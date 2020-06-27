6eac4ead65be7f4c05969028d770d849b4056d01
senadakadric <skadric@fordham.edu>
2020-06-26 22:14:14 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -13,3 +13,73 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+const toyCollectionParentNode = document.querySelector("#toy-collection");
+
+fetch("http://localhost:3000/toys")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (json) {
+    json.forEach(function (toy) {
+      addToyToDom(toy);
+    });
+  });
+
+function addToyToDom(toy) {
+  const toyDiv = document.createElement("div");
+  toyDiv.className = "card";
+
+  toyDiv.innerHTML = `
+      <h2>${toy.name}</h2>
+      <img src=${toy.image} class="toy-avatar" />
+      <p><span>${toy.likes}</span> Likes </p>
+      <button class="like-btn">Like <3</button>
+      `;
+
+  toyCollectionParentNode.append(toyDiv);
+
+  const button = toyDiv.querySelector("button");
+  button.addEventListener("click", function (event) {
+    // toy.likes++;
+    const toyLikes = toyDiv.querySelector("span");
+    toyLikes.innerText++;
+
+    fetch(`http://localhost:3000/toys/${toy.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        likes: toyLikes.innerText,
+      }),
+    });
+  });
+}
+
+const formElement = document.querySelector("form");
+
+formElement.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const name = event.target.name.value;
+  const image = event.target.name.value;
+
+  const toyObject = {
+    name: name,
+    image: image,
+    likes: 0,
+  };
+
+  fetch("http://localhost:3000/toys", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(toyObject),
+  });
+
+  addToyToDom(toyObject);
+});
