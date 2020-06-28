754c818fd8c38ee7e4e6c0f8218e6b18f9c55ea5
jukebox101 <leonora.squires@gmail.com>
2020-06-27 22:50:09 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -12,4 +12,90 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  //fetches all toys
+  function fetchToys(url) {
+    fetch(url) 
+      .then(response => response.json())
+      .then(toyArr => createElements(toyArr))
+  }
+  
+  fetchToys('http://localhost:3000/toys')
+  
+  const toyCollection = document.querySelector("#toy-collection");
+  const form = document.querySelector(".add-toy-form");
+
+  //renders each toy card
+  function renderToy (toyObj){
+   const toyDiv = document.createElement("div")
+
+   toyDiv.classList = "card"
+
+   toyDiv.innerHTML = `
+   <h2>${toyObj['name']}</h2>
+   <img src=${toyObj['image']} class="toy-avatar" />
+   <p>${toyObj['likes']} likes</p>
+   <button class="like-btn">Like <3</button>
+   `
+   const likeBtn = toyDiv.querySelector(".like-btn")
+   const likeP = toyDiv.querySelector("p")
+
+   // update likes event listener
+   likeBtn.addEventListener("click", () => {
+      toyObj.likes++
+      likeP.textContent = `${toyObj.likes} Likes`
+
+      fetch(`http://localhost:3000/toys/${toyObj.id}`, {
+        method: 'PATCH', // or 'PUT'
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: "application/json"
+        },
+        body: JSON.stringify({
+          "likes": toyObj.likes
+        })
+      })
+
+   })
+   toyCollection.append(toyDiv)
+  }
+
+  //renders all toy cards
+  function createElements (toyArr){
+    for (i = 0; i < toyArr.length; i++) {
+      renderToy(toyArr[i])
+    }
+    // toyArr.forEach(renderToy)
+  }
+
+  //new toy form event listener
+  form.addEventListener("submit", event => {
+    event.preventDefault()
+    
+    const name = event.target.name.value
+    const image = event.target.image.value
+    
+    console.log(event)
+  
+    fetch(`http://localhost:3000/toys`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+          "name": name,
+          "image": image,
+          "likes": 0
+        })
+        
+    })
+      .then(response => response.json())
+      .then(newToyObj => {
+        console.log(newToyObj)
+        createElements(newToyObj)
+      })
+      
+
+})
 });
